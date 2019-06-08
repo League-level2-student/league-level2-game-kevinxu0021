3119fed975a8a929329f96873a830670a4fa57c7
league <league@imac.attlocal.net>
2019-06-08 12:58:39 -0700

    Level 2 Project

---
+++ src/default_package/Dropper.java
@@ -0,0 +1,33 @@
+package default_package;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Dropper {
+	final static int width = 500;
+	final static int height = 900;
+	JFrame frame;
+	Panel panel;
+
+	Dropper() {
+		frame = new JFrame();
+		panel = new Panel();
+	}
+
+	public static void main(String[] args) {
+		Dropper dropper = new Dropper();
+		dropper.setup();
+	}
+
+	void setup() {
+		frame.add(panel);
+		frame.addKeyListener(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.getContentPane().setPreferredSize(new Dimension(width, height));
+		frame.setVisible(true);
+		frame.pack();
+
+	}
+}
